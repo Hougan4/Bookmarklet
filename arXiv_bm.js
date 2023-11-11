9dc7b7bbbfbb1609f630f0fbc52d566eac7971ef
Hougan4 <106863979+Hougan4@users.noreply.github.com>
2023-11-11 13:05:56 +0900

    Update arXiv_bm.js

--- arXiv_bm.js
+++ arXiv_bm.js
@@ -1,1 +1,1 @@
-(function() { var currentURL = window.location.href; var modifiedURL = currentURL.replace("arxiv", "ar5iv"); window.location.href = modifiedURL; })();
+javascript:(function() {  var currentURL = window.location.href;  var modifiedURL = currentURL.replace("arxiv", "ar5iv");  window.location.href = modifiedURL;})();
